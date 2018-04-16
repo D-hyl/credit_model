3ee8c679d3204d01afbb8835b7bc2a2fe0d264db
[username] <[email]>
2018-04-16 11:19:20 +0800

    update setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='creditmodel',
-    version=<1.0>,
+    version=1.0,
     description=(
         'create a credit card by logistic'
     ),
